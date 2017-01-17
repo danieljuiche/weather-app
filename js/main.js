0a46a9bcf9675197e9dcff74a6dc83a275e367f3
Captain Lin <Captain Lin>
2017-01-17 01:41:12 -0800

    Added data updates to client display

--- js/main.js
+++ js/main.js
@@ -11,34 +11,15 @@ var processData = function (data) {
 		var latLocation = data.latitude;
 		// Store Longitude Location
 		var longLocation = data.longitude;
+
+		updateLocation(latLocation, longLocation);
 		// Store Unit
 		var unitDefault = "metric"
 
-
 		// Retrieve information for current weather conditions
-		$.getJSON("http://api.openweathermap.org/data/2.5/weather?lat=" + latLocation + "&lon=" + longLocation + "&appid=" + apiKey + "&units=" + unitDefault).done (function (currentWeather) {
-			console.log(currentWeather);
-			
-			// Get location information
-			var currentCity = currentWeather.name;
-
-			// Get Temperature Information
-			var currentTemp = Math.round(currentWeather.main.temp);
-			console.log("The current temperature is " + currentTemp);
-			var currentHumidity = currentWeather.main.humidity;
-			console.log("The current humidty is " + currentHumidity);
-			var currentCloudiness = currentWeather.clouds.all;
-			console.log("The current cloudiness is " + currentCloudiness);
-
-			// Get current weather condition
-			var currentConditionsId = currentWeather.weather[0].id;
-			console.log("Current conditions ID is: " + currentConditionsId);
-			var currentConditionsDescription = currentWeather.weather[0].description;
-			console.log("Current conditions is : " + currentConditionsDescription);
-			// Display current weather condition
-				// Display humidity
-				// Display temperature
-					// Perform C to F conversion
+		$.getJSON("http://api.openweathermap.org/data/2.5/weather?lat=" + latLocation + "&lon=" + longLocation + "&appid=" + apiKey + "&units=" + unitDefault).done (function (currentWeatherData) {
+			console.log(currentWeatherData);
+			updateWeatherConditions(currentWeatherData);
 		});
 			
 	console.log(data);
@@ -61,3 +42,58 @@ var approximateLocation = function () {
 		processData(location);
 	});
 };
+
+var updateWeatherConditions = function (currentWeatherData) {
+	// Get location information
+	var currentCity = currentWeatherData.name;
+	var currentCountry = currentWeatherData.sys.country;
+
+	// Get time information in UNIX
+	var currentTime = timeConversion(currentWeatherData.dt);
+	updateTime(currentTime);
+
+	// Get Temperature Information
+	var currentTemp = Math.round(currentWeatherData.main.temp);
+	// console.log("The current temperature is " + currentTemp);
+	var currentHumidity = currentWeatherData.main.humidity;
+	// console.log("The current humidty is " + currentHumidity);
+	var currentCloudiness = currentWeatherData.clouds.all;
+	// console.log("The current cloudiness is " + currentCloudiness);
+
+	// Get current weather condition
+	var currentConditionsId = currentWeatherData.weather[0].id;
+	// console.log("Current conditions ID is: " + currentConditionsId);
+	var currentConditionsDescription = currentWeatherData.weather[0].description;
+	// console.log("Current conditions is : " + currentConditionsDescription);
+
+	$('.city-country').html(currentCity + ", " + currentCountry);
+	$('.temperature').html("Temperature: " + currentTemp + " &deg;C");
+	$('.humidity').html("Humidity: " + currentHumidity + "%");
+	$('.cloudiness').html("Cloudiness: " + currentCloudiness + "%");
+}
+
+var updateLocation = function (latitude, longitude) {
+	$('.latitude').html("Latitude: " + latitude);
+	$('.longitude').html("Longitude: " + longitude);
+}
+
+var timeConversion = function (unixTime) {
+	var date = new Date(unixTime*1000);
+	// Hours part from the timestamp
+	var hours = date.getHours();
+	if (hours <= 9) {
+		hours = "0" + hours;
+	}
+	// Minutes part from the timestamp
+	var minutes = "0" + date.getMinutes();
+	// Seconds part from the timestamp
+	var seconds = "0" + date.getSeconds();
+
+	// Will display time in 10:30:23 format
+	var formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+	return formattedTime;
+}
+
+var updateTime = function (currentTime) {
+	$('.time').html("Time: " + currentTime);
+}
