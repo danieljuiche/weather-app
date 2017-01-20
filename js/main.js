52863d5187dca4bf0b310a1fa42e15ad1854554f
Captain Lin <Captain Lin>
2017-01-20 14:51:13 -0800

    Fixed mobile and desktop handlers

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 $(document).ready(function () {
 	if ( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-		approximateLocation();
+		mobileDeviceHandler();
 	}
 	else {
-		mobileDeviceHandler();
+		approximateLocation();
 	}
 });
 
